95e81a1ccb8e85d312225767d2f9b30196451c6b
user1 <user1@DESKTOP-UNR49SD>
2025-10-18 15:57:39 +0300

    Тема 2: LinkedList<T> 1 задача

--- App/Topics/LinkedList/T2_LinkedList/Task.cs
+++ App/Topics/LinkedList/T2_LinkedList/Task.cs
@@ -1,1 +1,41 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
 namespace App.Topics.LinkedList.T2_LinkedList;
+
+public static class LinkedListTasks
+{
+    public static LinkedList<int> RemoveDuplicates(LinkedList<int> list)
+    {
+        if (list == null) 
+            throw new ArgumentNullException("list");
+
+        if (list.Count == 1)
+            return new LinkedList<int>(list);
+
+        var result = new LinkedList<int>();
+        var seenValues = new HashSet<int>();
+
+        
+        var currentNode = list.First;
+        while (currentNode != null)
+        {
+            int currentValue = currentNode.Value;
+
+           
+            if (!seenValues.Contains(currentValue))
+            {
+                seenValues.Add(currentValue);
+                result.AddLast(currentValue);
+            }
+             
+            currentNode = currentNode.Next;
+        }
+
+        return result;
+    }
+}
+
+    
