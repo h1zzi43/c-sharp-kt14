afb951600f1aef8981e6d4bdd0fc8251cad2bc0a
h1zzi43 <avedeneev20@gmail.com>
2025-12-05 15:13:23 +0300

    Dictionary<TKey, TValue>

--- App/Topics/Dictionary/T3_Dictionary/Task.cs
+++ App/Topics/Dictionary/T3_Dictionary/Task.cs
@@ -1,1 +1,55 @@
 namespace App.Topics.Dictionary.T3_Dictionary;
+
+public static class DictionaryTasks
+{
+    public static List<KeyValuePair<string, int>> TopNWords(string text, int n)
+    {
+        if (string.IsNullOrEmpty(text) || n <= 0)
+        {
+            return new List<KeyValuePair<string, int>>();
+        }
+
+        var wordFrequencies = new Dictionary<string, int>();
+        string currentWord = "";
+
+        foreach (char c in text)
+        {
+            if (char.IsLetterOrDigit(c))
+            {
+                currentWord += c;
+            }
+            else if (currentWord.Length > 0)
+            {
+                string normalizedWord = currentWord.ToLower();
+                if (wordFrequencies.ContainsKey(normalizedWord))
+                {
+                    wordFrequencies[normalizedWord]++;
+                }
+                else
+                {
+                    wordFrequencies[normalizedWord] = 1;
+                }
+                currentWord = "";
+            }
+        }
+
+        if (currentWord.Length > 0)
+        {
+            string normalizedWord = currentWord.ToLower();
+            if (wordFrequencies.ContainsKey(normalizedWord))
+            {
+                wordFrequencies[normalizedWord]++;
+            }
+            else
+            {
+                wordFrequencies[normalizedWord] = 1;
+            }
+        }
+
+        return wordFrequencies
+            .OrderByDescending(kv => kv.Value)
+            .ThenBy(kv => kv.Key)
+            .Take(n)
+            .ToList();
+    }
+}
