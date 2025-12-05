4ceec6fcd6e5e72c7f9e122af77648ba5ff59fbe
h1zzi43 <avedeneev20@gmail.com>
2025-12-05 15:11:44 +0300

    Доп. задача: Двусвязный список

--- App/Topics/LinkedList/T2b_DoubleLinkedList/Task.cs
+++ App/Topics/LinkedList/T2b_DoubleLinkedList/Task.cs
@@ -1,1 +1,102 @@
 namespace App.Topics.LinkedList.T2b_DoubleLinkedList;
+
+public class DoubleLinkedList<T>
+{
+    public T Value { get; }
+    public DoubleLinkedList<T>? Prev { get; private set; }
+    public DoubleLinkedList<T>? Next { get; private set; }
+
+    public DoubleLinkedList(T value)
+    {
+        Value = value;
+    }
+
+    public int Count
+    {
+        get
+        {
+            var head = this;
+            while (head.Prev != null)
+            {
+                head = head.Prev;
+            }
+
+            int count = 0;
+            var current = head;
+            while (current != null)
+            {
+                count++;
+                current = current.Next;
+            }
+
+            return count;
+        }
+    }
+
+    public void AddBefore(T value)
+    {
+        var newNode = new DoubleLinkedList<T>(value);
+        newNode.Prev = Prev;
+        newNode.Next = this;
+
+        if (Prev != null)
+        {
+            Prev.Next = newNode;
+        }
+        Prev = newNode;
+    }
+
+    public void AddAfter(T value)
+    {
+        var newNode = new DoubleLinkedList<T>(value);
+        newNode.Prev = this;
+        newNode.Next = Next;
+
+        if (Next != null)
+        {
+            Next.Prev = newNode;
+        }
+        Next = newNode;
+    }
+
+    public void AddFirst(T value)
+    {
+        var head = this;
+        while (head.Prev != null)
+        {
+            head = head.Prev;
+        }
+        head.AddBefore(value);
+    }
+
+    public void AddLast(T value)
+    {
+        var tail = this;
+        while (tail.Next != null)
+        {
+            tail = tail.Next;
+        }
+        tail.AddAfter(value);
+    }
+
+    public T[] ToArray()
+    {
+        var head = this;
+        while (head.Prev != null)
+        {
+            head = head.Prev;
+        }
+
+        var result = new T[Count];
+        var current = head;
+        int index = 0;
+
+        while (current != null)
+        {
+            result[index++] = current.Value;
+            current = current.Next;
+        }
+
+        return result;
+    }
+}
